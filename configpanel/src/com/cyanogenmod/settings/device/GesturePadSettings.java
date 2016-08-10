176f1fc07f1e01e8620532d96754c5ace0cc0d72
Blackjubel <blackjubels@gmail.com>
2016-08-10 05:38:28 +0200

    Update GesturePadSettings.java

--- configpanel/src/com/cyanogenmod/settings/device/GesturePadSettings.java
+++ configpanel/src/com/cyanogenmod/settings/device/GesturePadSettings.java
@@ -16,6 +16,7 @@
 
 package com.cyanogenmod.settings.device;
 
+import com.android.internal.util.cm.ScreenType;
 import com.cyanogenmod.settings.device.utils.Constants;
 import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
 
